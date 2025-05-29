88cfa5631410dd4e3aa1cfe41e457ec5dfa99bdb
Ganesh <ganeshgopi2612@gmail.com>
2025-05-29 16:31:11 +0530

    [Ganesh] add: Pipes

---
+++ Day4/Pipes/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Welcome to Angular';
+  toDate=new Date();
+  items=['item1','item2','item3','item4'];
+  price=500;
+}
